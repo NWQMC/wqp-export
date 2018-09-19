5e4839e60b9c34af5537453ed1653b030617713e
David Uselmann <duselmann@usgs.gov>
2018-09-19 10:07:44 -0500

    export as CSV using openCSV

--- src/main/java/gov/usgs/water/control/Service.java
+++ src/main/java/gov/usgs/water/control/Service.java
@@ -1,6 +1,12 @@
 package gov.usgs.water.control;
 
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,6 +15,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.opencsv.CSVWriter;
+
+import gov.usgs.water.app.AppConfig;
 import gov.usgs.water.app.SwaggerConfig;
 import io.swagger.annotations.ApiOperation;
 
@@ -41,12 +50,35 @@ public class Service {
 			LOGGER.trace("exited: good");
 			return csv;
 		} catch (Exception e) {
-			LOGGER.trace("exited: b");
+			LOGGER.trace("exited: bad");
 		}
 		return "";
 	}
 
 
+	@ApiOperation(
+			value = "Export Service",
+			notes = SwaggerConfig.StatsService_EXPORT_NOTES
+		)
+	@GetMapping(value = "/fileExport",
+			produces = "text/csv"
+		)
+	public String fileExport() {
+
+		try {
+			LOGGER.trace("entered");
+
+			String csv = executeExport(AppConfig.getExportFileName());
+
+			LOGGER.trace("exited: good");
+			return csv;
+		} catch (Exception e) {
+			LOGGER.trace("exited: bad");
+		}
+		return "";
+	}
+	
+	
 	@ApiOperation(
 			value = "Count Service",
 			notes = SwaggerConfig.StatsService_EXPORT_NOTES
@@ -64,7 +96,7 @@ public class Service {
 			LOGGER.trace("exited: good");
 			return count;
 		} catch (Exception e) {
-			LOGGER.trace("exited: b");
+			LOGGER.trace("exited: bad");
 		}
 		return "";
 	}
@@ -72,7 +104,7 @@ public class Service {
 
 	public String executeCount() throws Exception {
 
-		LOGGER.info("exporting data");
+		LOGGER.info("getting row count");
 
 		String result = jdbcTemplate.query(
 			"select count(*) as total from web_service_log",
@@ -89,23 +121,51 @@ public class Service {
 		return result;
 	}
 
-
 	public String executeExport() throws Exception {
+		return executeExport(null);
+	}
 
-		LOGGER.info("exporting data");
+	public String executeExport(String toFile) throws Exception {
+		// outside the try resource management is okay because OpenCSV closes parents
+		Writer writer;
+		if (StringUtils.isNotBlank(toFile)) {
+			File file = new File(toFile);
+			LOGGER.info("exporting data to file: {}", file.getAbsolutePath());
+			writer = new FileWriter(file);
+		} else {
+			LOGGER.info("exporting data");
+			writer = new StringWriter();
+		}
 
 		String result = jdbcTemplate.query(
-			"select * from web_service_log",
+				// TODO remove 5 restriction
+			"select * from web_service_log where rownum < 5",
 			rs -> {
-				StringBuilder builder = new StringBuilder();
-				while (rs.next()) {
-					builder.append( rs.getString("") );
+				int cols = rs.getMetaData().getColumnCount();
+				String[] line = new String[cols];
+
+				try (CSVWriter csv = new CSVWriter(writer)) {
+					while (rs.next()) {
+						for (int col=0; col<cols;) {
+							line[col] = rs.getString(++col);
+						}
+						csv.writeNext(line, false);
+					}
+					csv.flush();
+					writer.flush();
+					return "success";
+				} catch (Exception e) {
+					e.printStackTrace();
+					return "error writing csv file  " + e.getMessage();
 				}
-				return builder.toString();
 			}
 		);
 
-		LOGGER.debug(result);
+		if ("success".equals(result) && writer instanceof StringWriter) {
+			StringWriter string = (StringWriter) writer;
+			result = string.getBuffer().toString();
+		}
+		LOGGER.trace("entries\n{}",result);
 		return result;
 	}
 
