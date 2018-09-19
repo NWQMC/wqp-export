996d76c10bbc28186290b58b6d28428d927fbabc
David Uselmann <duselmann@usgs.gov>
2018-09-19 10:05:35 -0500

    unfortunate eclipse spaces to tabs, added export file name and logging

--- src/main/java/gov/usgs/water/app/AppConfig.java
+++ src/main/java/gov/usgs/water/app/AppConfig.java
@@ -2,6 +2,7 @@ package gov.usgs.water.app;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,39 +16,51 @@ import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
 @Configuration
 @ComponentScan
 @EnableTransactionManagement
 @PropertySource(value = { "classpath:application.properties" })
 public class AppConfig {
+	private static Logger LOGGER = org.slf4j.LoggerFactory.getLogger(AppConfig.class);
+
+	private static String exportFileName;
+	public static String getExportFileName() {
+		return exportFileName;
+	}
 
 	@Autowired
-    private Environment env;
+	private Environment env;
  
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }    
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
  
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
-    }
+	@Bean
+	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
+	}
  
-    @Bean
-    public PlatformTransactionManager transactionManager(DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
-    }
+	@Bean
+	public PlatformTransactionManager transactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
  
-    @Bean
-    public DataSource dataSource() {
-    	System.err.println("Setting up data source");
-    	
-    	SingleConnectionDataSource ds = new SingleConnectionDataSource();
-    	ds.setDriverClassName(oracle.jdbc.driver.OracleDriver.class.getName());
-        ds.setUrl(env.getProperty("jdbc.url"));
-        ds.setUsername(env.getProperty("jdbc.username"));
-        ds.setPassword(env.getProperty("jdbc.password"));
-        return ds;
-    }
+	@Bean
+	public DataSource dataSource() {
+		exportFileName = env.getProperty("export.filename", "wqp-default-export.csv");
+		LOGGER.info("Export File Name: {}", exportFileName);
+
+		String jdbcURL = env.getProperty("jdbc.url", "not specified");
+		LOGGER.info("Setting up data source: {}", jdbcURL);
+
+		SingleConnectionDataSource ds = new SingleConnectionDataSource();
+		ds.setDriverClassName(oracle.jdbc.driver.OracleDriver.class.getName());
+		ds.setUrl(jdbcURL);
+		ds.setUsername(env.getProperty("jdbc.username"));
+		ds.setPassword(env.getProperty("jdbc.password"));
+		
+		return ds;
+	}
 }
